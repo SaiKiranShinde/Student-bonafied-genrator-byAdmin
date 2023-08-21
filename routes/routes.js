b4719284f3891a5c07fe6df6771bfa3e59421dbc
Sai Kiran <saikiranshinde683@gmail.com>
2023-08-22 04:38:34 +0530

    Initial files;

--- routes/routes.js
+++ routes/routes.js
@@ -5,7 +5,12 @@ routes.get("/", (req, res) => {
   if (req.cookies["www.student.com"]) return res.render("user/dashboard");
   if (req.cookies["www.admin.com"]) return res.render("admin/dashboard");
   res.render("login", {message:""});
+});
 
+routes.get("/login", (req, res) => {
+  if (req.cookies["www.student.com"]) return res.render("user/dashboard");
+  if (req.cookies["www.admin.com"]) return res.render("admin/dashboard");
+  res.render("login", {message:""});
 });
 
 module.exports = routes;
